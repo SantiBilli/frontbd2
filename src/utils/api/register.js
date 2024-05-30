e3f7abed0f95e758f13cc77afd6cd5744bd3ad32
SantiBilli <santifbill@gmail.com>
2024-05-30 11:59:32 -0300

    navchanges

--- src/utils/api/register.js
+++ src/utils/api/register.js
@@ -1,8 +1,8 @@
 export const sendRegisterForm = async (credentials) => {
     
-    console.log(credentials);
+    //console.log(credentials);
 
-    const response = await fetch("http://localhost:3500/api/register", {
+    const response = await fetch("http://localhost:3500/api/register/", {
         method: "POST",
         mode: "cors",
         headers: {"Content-Type": "application/json"},
